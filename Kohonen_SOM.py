9122ba45f6f8386013c7e503bd06a31d88d2e6f3
SpaceMeerkat <james.d.astro@gmail.com>
2018-09-21 16:30:49 +0100

    First commit

---
+++ Kohonen_SOM.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 13:46:44 2017
+
+@author: SPACEMEERKAT
+"""
+import numpy as np
+from random import randint
+import random
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+plt.close('all')
+
+def Kohonen_Layer(x_size,y_size,your_data):
+        k_layer = np.random.uniform(your_data.min(),your_data.max(),(x_size,y_size,your_data.shape[-1]))
+        return k_layer
+
+def BMU_node(k_layer,data_array):
+        flat_w = np.vstack(np.copy(k_layer))
+        BMU_val = np.linalg.norm(data_array-flat_w,axis=1).argmin()
+        BMU = [int(np.floor(BMU_val/float(k_layer.shape[1]))),int(BMU_val-(np.floor((BMU_val/float(k_layer.shape[1])))*k_layer.shape[1]))]
+        return BMU
+
+def lambda_function(initial_radius,number_of_iterations):
+        return number_of_iterations/np.log(initial_radius)
+        
+def radial_decay_function(time_step,lambda_value,initial_radius):
+        return initial_radius*np.exp(-time_step/lambda_value)
+
+def learning_rate(initial_learning_rate,time_step,lambda_value):
+        return initial_learning_rate*np.exp(-time_step/lambda_value)
+
+def node_influence(node_distances,radius):
+        return np.exp((-(node_distances**2))/(2*(radius**2)))
+
+def weight_update(k_layer,theta,L,data,weights,weight_rows,weight_cols):
+        k_layer[weight_rows,weight_cols] += (theta*L*(data-weights).T).T
+        return k_layer
+
+def evaluate(test_data,trained_map):
+        cols = []
+        rows = []
+        for i in range(test_data.shape[0]-1):
+                bmu = BMU_node(trained_map,test_data[i])
+                cols.append(bmu[0])
+                rows.append(bmu[1])
+        return np.hstack(np.array(cols)), np.hstack(np.array(rows))
+                
+
+""" GENERATING TEST DATA """
+test_data = np.array([[0,0,1],[0,1,0],[1,0,0],[1,1,1],[0,0,0],[1,1,0],[1,0,1],[0,0,1]])
+
+def training(x_size,y_size,your_data,initial_radius,number_of_iterations,initial_learning_rate):        
+        k_layer = Kohonen_Layer(x_size,y_size,your_data)
+        data_length = np.arange(0,your_data.shape[0]-1,1)
+        bmu_list = []
+        cols_list = []
+        rows_list = []
+        for i in tqdm(range(int(number_of_iterations))):
+                random.shuffle(data_length)
+                for j in range(len(data_length)):
+                        data_array = your_data[data_length[j]]
+                        lambda_value = lambda_function(initial_radius,number_of_iterations)
+                        radius = radial_decay_function(i,lambda_value,initial_radius)                
+                        BMU = BMU_node(k_layer,data_array)
+                        bmu_list.append(BMU)
+                        indices = np.indices((k_layer.shape[0],k_layer.shape[1])).reshape(2, -1).T
+                        nodes_in_r = indices[np.where(np.linalg.norm(BMU-indices,axis=1)<radius)[0]]
+                        node_dists = np.linalg.norm(nodes_in_r-BMU,axis=1)
+                        rows = nodes_in_r[:,0]
+                        cols = nodes_in_r[:,1]
+                        cols_list.append(cols)
+                        rows_list.append(rows)
+                        node_values = k_layer[rows,cols]
+                        theta = node_influence(node_dists,radius)
+                        learn_rate = learning_rate(initial_learning_rate,i,lambda_value)
+                        k_layer = weight_update(k_layer,theta,learn_rate,data_array,node_values,rows,cols)
+        return k_layer
+
+""" TRAINING THE SOM ON OUR TEST DATA """
+som = training(x_size=100,y_size=100,your_data=test_data,initial_radius=100,number_of_iterations=100,initial_learning_rate=0.1)
+
+""" PLOTTING THE FINAL SOM """
+plt.figure()
+for i in tqdm(range(som.shape[0])):
+        for  j in range(som.shape[1]):
+                plt.plot(j,i,'.',color=som[j,i],markersize=10.0)
+                
+test_cols, test_rows = evaluate(test_data,som)
+plt.plot(test_cols,test_rows,'k^')
+
+           
+        
+
+
+
+
+
+         
+
