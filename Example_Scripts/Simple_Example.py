eff03de76d84f5a7c1283105c4a34af297183c6a
SpaceMeerkat <32263989+SpaceMeerkat@users.noreply.github.com>
2018-10-23 11:20:48 +0100

    Update Simple_Example.py

--- Example_Scripts/Simple_Example.py
+++ Example_Scripts/Simple_Example.py
@@ -13,13 +13,15 @@ from tqdm import tqdm
 from Kohonen_SOM import SOM
 
 #Generate data
-train_data = np.array([[0,0,1],[0,1,0],[1,0,0],[1,1,1],[0,0,0],[1,1,0],[1,0,1],[0,0,1],[0,0,0.8],[0,0,0.91],[0,0,0.95],[0,0,0.949]]) 
+train_data = np.array([[0,0,1],[0,1,0],[1,0,0],[1,1,1],[0,0,0],[1,1,0],[1,0,1],
+                       [0,0,1],[0,0,0.8],[0,0,0.91],[0,0,0.95],[0,0,0.949]]) 
           
 #Instantiate the SOM classs
 som_maker = SOM() 
 
 #Train the SOM
-som = SOM.generate_SOM(som_maker,x_size=100,y_size=100,your_data=train_data,initial_radius=100,number_of_iterations=100,initial_learning_rate=0.1)       
+som = SOM.generate_SOM(som_maker,x_size=100,y_size=100,your_data=train_data,initial_radius=100,
+                       number_of_iterations=100,initial_learning_rate=0.1)       
 
 #Evaluate data on the trained map
 tested_data = SOM.evaluate(som_maker,train_data,som)
